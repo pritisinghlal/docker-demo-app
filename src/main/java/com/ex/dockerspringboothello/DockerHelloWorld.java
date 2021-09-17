1e30db27d309923c184bc3f10118372f98de6465
priti singh <pritisinghlal@gmail.com>
2021-09-17 16:32:12 -0400

    test

--- src/main/java/com/ex/dockerspringboothello/DockerHelloWorld.java
+++ src/main/java/com/ex/dockerspringboothello/DockerHelloWorld.java
@@ -4,13 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/v1/docker")
-public class DockerHelloWorld {
-
-	@GetMapping
-	public String getName() {
-		
-		return "Welcome to deployment in docker container";
-	}
-}
+/*
+ * @RestController
+ * 
+ * @RequestMapping("/v1/docker") public class DockerHelloWorld {
+ * 
+ * @GetMapping public String getName() {
+ * 
+ * return "Welcome to deployment in docker container"; } }
+ */
--- src/main/java/com/ex/dockerspringboothello/DockerSpringbootHelloApplication.java
+++ src/main/java/com/ex/dockerspringboothello/DockerSpringbootHelloApplication.java
@@ -2,8 +2,10 @@ package com.ex.dockerspringboothello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.ex.dockerspringboothello")
 public class DockerSpringbootHelloApplication {
 
 	public static void main(String[] args) {
